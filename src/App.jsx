9f322b6c1c822526613a4690f79316d8744e889a
mukul sharma <109076009+mukulsh888@users.noreply.github.com>
2025-11-05 09:36:15 +0530

    'pushedaftertoaster'

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "@/pages/Home"
 import NotFound from "@/pages/NotFound"
-import { Toaster } from "@/components/UI/Toaster"
 
+import {Toaster} from '@/components/UI/toaster'
 
 function App() {
   
