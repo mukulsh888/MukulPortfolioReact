d2a91e371535919dbf8ec6030e174f06e7e97c37
mukul sharma <109076009+mukulsh888@users.noreply.github.com>
2025-11-05 09:29:22 +0530

    'pushingafterfixingbuild'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import NotFound from "./pages/NotFound"
-import { Toaster } from "./components/UI/Toaster"
+import Home from "@/pages/Home"
+import NotFound from "@/pages/NotFound"
+import { Toaster } from "@/components/UI/Toaster"
 
 
 function App() {
